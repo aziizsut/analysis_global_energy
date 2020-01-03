90d54c2da1b8c8ca59fc30caa658e799322c794f
Aziiz <aziiz@sutrisno@gmail.com>
2020-01-03 11:39:28 +0100

    plotting scripts

---
+++ perbaikan_non_submission.R
@@ -0,0 +1,37 @@
+table_indegree
+pld$data[[1]]
+table_indegree <- table_indegree %>% 
+  mutate(gas_indegree = if_else(is.infinite(gas_indegree), lag(gas_indegree), gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(is.infinite(gas_indegree), lead(gas_indegree), gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(is.infinite(gas_indegree), lag(gas_indegree), gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(is.infinite(gas_indegree), lead(gas_indegree), gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(is.infinite(gas_indegree), lag(gas_indegree), gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(is.infinite(gas_indegree), lead(gas_indegree), gas_indegree)) %>% 
+  mutate(period = as.numeric(period)) %>% 
+  mutate(gas_indegree = if_else(importer == "SGP" & period >= 2006 & period <= 2010, 12, gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(importer == "NLD" & period == 2015, 12, gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(importer == "NLD" & period == 2016, 17, gas_indegree)) %>% 
+  mutate(gas_indegree = if_else(importer == "NLD" & period == 2017, 23, gas_indegree))
+
+ggplot(data = table_indegree, aes(x = period, y = coal_indegree)) + geom_line() + facet_wrap(~importer)
+table_indegree %>% filter(importer == "NLD") %>% tail(n = 15L)
+table_indegree %>% filter(importer == "NLD") %>% tail(n = 15L)
+coba2 <- coba %>% select(period, importer, gas_indegree)
+
+table_indegree <- table_indegree %>% select(-gas_indegree) 
+table_indegree <- left_join(table_indegree, coba2) %>% select(period, importer, coal_indegree, gas_indegree, oil_indegree)
+
+table_indegree <- table_indegree %>% 
+  mutate(oil_indegree = if_else(is.infinite(oil_indegree), lag(oil_indegree), oil_indegree)) %>% 
+  mutate(oil_indegree = if_else(is.infinite(oil_indegree), lead(oil_indegree), oil_indegree)) %>% 
+  mutate(oil_indegree = if_else(is.infinite(oil_indegree), lag(oil_indegree), oil_indegree)) %>% 
+  mutate(oil_indegree = if_else(is.infinite(oil_indegree), lead(oil_indegree), oil_indegree)) %>% 
+  mutate(oil_indegree = if_else(is.infinite(oil_indegree), lag(oil_indegree), oil_indegree)) %>% 
+  mutate(oil_indegree = if_else(is.infinite(oil_indegree), lead(oil_indegree), oil_indegree)) %>% 
+  mutate(oil_indegree = if_else(importer == "NLD" & period >= 2015 & period <= 2017, 44, oil_indegree))
+
+
+table_indegree <- table_indegree %>% 
+  mutate(coal_indegree = if_else(importer == "NLD" & period == 2016, 31, coal_indegree)) %>% 
+  mutate(coal_indegree = if_else(importer == "NLD" & period == 2015, 30, coal_indegree)) %>% 
+  mutate(coal_indegree = if_else(importer == "NLD" & period == 2017, 27, coal_indegree))
--- prepDependentVariables.R
+++ prepDependentVariables.R
@@ -3,8 +3,36 @@
 #' load the Data generated from dataLoadWrangler and global wrangling
 #' 
 
+# Dataset Preparation. ----------------------------------------------------
+#' This section allows user to set up the dataset into the format that is suitable for analysis to run
+#' This takes the original value of the import data without filter to the exporter list
+#' pld is coal dataset name
+#' pld_gas is gas dataset name
+#' pld_oil is for the oil dataset
+
+# Prepare the Coal Dataset
+pld <- test_coal %>% select(original_period, exporter, importer, value) %>% 
+  rename(period = original_period) %>% 
+  select(period, everything()) %>%
+  group_by(period, importer) %>% 
+  nest()
+
+# Prepare the Gas Dataset
+pld_gas <- test_gas %>% select(original_period, exporter, importer, value) %>% 
+  rename(period = original_period) %>% 
+  select(period, everything()) %>%
+  group_by(period, importer) %>% 
+  nest()
+
+# Prepare the Oil Dataset
+pld_oil <- test_oil %>% select(original_period, exporter, importer, value) %>% 
+  rename(period = original_period) %>% 
+  select(period, everything()) %>%
+  group_by(period, importer) %>% 
+  nest()
+
 # Ego Network Connections -------------------------------------------------
-run_row <- seq(1:nrow(pld))
+run_row <- seq(1:nrow(pld)) # lookup the number of the row for map operation
 
 
 # This section used to calculate the indegree == trade connections --------
@@ -15,64 +43,57 @@ find_indegree <- function(cumi){
   #' This function use igraph function to find the indegree of importer countries at time x
   #' Please don't forget to change the dataset value (initially named pld) according to the commodities
   pld_oil$data[[cumi]] %>% mutate(importer = pld_oil$importer[cumi]) %>% select(exporter, importer, value) %>% 
-    filter(value > 0) %>% graph_from_data_frame() %>% degree(V(.), mode = "in") %>% max() #Translate to numeric because it returns a list
-  }
+    filter(value > 0) %>% filter(!exporter %in% rem_exporter_list) %>% 
+  graph_from_data_frame() %>% degree(V(.), mode = "in") %>% max() #Translate to numeric because it returns a list
+}
 
-# Prepare the Coal Dataset
-pld <- test_coal %>% select(original_period, exporter, importer, value) %>% 
-  rename(period = original_period) %>% 
-  select(period, everything()) %>%
-  group_by(period, importer) %>% 
-  nest()
 
 # Before running this remember to adapt the find_indegree data to pld
 coal_indegree <- map_dbl(run_row, find_indegree) %>% enframe(value = "coal_indegree") %>% select(-name)
 
-
-# Prepare the Gas Dataset
-pld_gas <- test_gas %>% select(original_period, exporter, importer, value) %>% 
-  rename(period = original_period) %>% 
-  select(period, everything()) %>%
-  group_by(period, importer) %>% 
-  nest()
-
 # Before running this remember to adapt the find_indegree data to pld_gas
 gas_indegree <- map_dbl(run_row, find_indegree) %>% enframe(value = "gas_indegree") %>% select(-name)
-
-
-# Prepare the Oil Dataset
-pld_oil <- test_oil %>% select(original_period, exporter, importer, value) %>% 
-  rename(period = original_period) %>% 
-  select(period, everything()) %>%
-  group_by(period, importer) %>% 
-  nest()
 
 # Before running this remember to adapt the find_indegree data to pld_oil
 oil_indegree <- map_dbl(run_row, find_indegree) %>% enframe(value = "oil_indegree") %>% select(-name)
 
-# Consolidation of all the indegree calculation
-table_indegree <- pld %>% select(period, importer) %>% bind_cols(coal_indegree, gas_indegree, oil_indegree) 
+#' at this point the Find indegree function uses pld_oil as the input data 
 
+# Consolidation of all the indegree calculation
+table_indegree <- pld %>% select(period, importer) %>% bind_cols(coal_indegree, gas_indegree, oil_indegree) # Using pld row structure to construct panel
+
+table_indegree %>% filter(is.infinite(oil_indegree))
 
 # Relative Balance Section ------------------------------------------------
 
-find_relB <- function(udang){
-  
-  pld_oil$data[[udang]] %>% filter(value > 0) %>% mutate(prop = value/sum(value),
-                                                     lProp = log(prop, base = 2),
-                                                     sums = prop * lProp) %>% 
-    summarise(hasil = 2^(-1 * sum(sums, na.rm = TRUE)))
+find_relB <- function(udang) {
+  # don't forget to always change the dataset to the commodity currently being analysed
+  # similar to the find indegree function
+  pld_oil$data[[udang]] %>% filter(value > 0) %>%
+    filter(!exporter %in% rem_exporter_list) %>%
+    mutate(
+      prop = value / sum(value),
+      lProp = log(prop, base = 2),
+      sums = prop * lProp
+    ) %>%
+    summarise(hasil = 2 ^ (-1 * sum(sums, na.rm = TRUE)))
   
 }
 
 # Don't forget to always change the dataset name (pld_oil for oil, pld_gas for gas, pld for coal)
-rBalance_oil <- map_dfr(run_row, find_relB)
-rBalance_gas <- map_dfr(run_row, find_relB)
 rBalance_coal <- map_dfr(run_row, find_relB)
+rBalance_gas <- map_dfr(run_row, find_relB)
+rBalance_oil <- map_dfr(run_row, find_relB)
+
+# At this point find_relB function use pld_oil as the input
 
 # We change the name of the columns based on the commodities
 table_rel_balance <- pld %>% select(period, importer) %>% bind_cols(rBalance_coal, rBalance_gas, rBalance_oil) %>% 
   rename(balance_coal = colnames(.)[3],
          balance_gas = colnames(.)[4],
-         balance_oil = colnames(.)[5])
+         balance_oil = colnames(.)[5]) %>% 
+  mutate(period = as.numeric(period))
+
+
+# please run the supplier turnover scripts first before joining all the table
 table_dependent <- left_join(table_indegree, table_rel_balance)
